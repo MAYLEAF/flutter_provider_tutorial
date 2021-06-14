28e03ed0c03d845742ea25e5c44976e008c08b9f
Mayleaf <uh.jeong651@gmail.com>
2021-06-15 08:13:07 +0900

    Feat: learn counter provider

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,147 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Provider<int>.value(
-      value:5,
-      child: MaterialApp(
-        home: SimplePage(),
+    return ChangeNotifierProvider<Counter>(
+        create: (_) => Counter(0),
+        child: MaterialApp(
+          title: "Flutter Value",
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: HomePage(),
+        ));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Provider demo'),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("${counter.getCounter()}",
+              style: Theme.of(context).textTheme.display1),
+          TextButton(
+            onPressed: openFirstPage,
+            child: Text('first page'),
+          ),
+          TextButton(
+            onPressed: openSecondPage,
+            child: Text('second page'),
+          )
+        ],
+      )),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: counter.increment,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+            heroTag: null,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            onPressed: counter.decrement,
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+            heroTag: null,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future openFirstPage() {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => FirstPage()),
+    );
+  }
+
+  Future openSecondPage() {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SecondPage()),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('first page'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('first page counter ${counter.getCounter()}'),
+        ),
       ),
     );
   }
 }
 
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
 
-class SimplePage extends StatelessWidget {
+class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    var data = Provider.of<int>(context);
-
+    final counter = Provider.of<Counter>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Simple app'),
+        title: Text('first page'),
       ),
       body: Center(
-        child: Text('${data}'),
-      )
+        child: Container(
+          child: Text('first page counter ${counter.getCounter()}'),
+        ),
+      ),
     );
   }
+}
 
+class Counter with ChangeNotifier {
+  int _counter;
 
+  Counter(this._counter);
 
+  getCounter() => _counter;
+  setCounter(int counter) => _counter = counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _counter--;
+    notifyListeners();
+  }
 }
