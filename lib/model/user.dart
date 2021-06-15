9ad8dab6d831f02927a5c5b521eb98254144e84f
Mayleaf <uh.jeong651@gmail.com>
2021-06-15 16:17:33 +0900

    Feat: Add user model

---
+++ lib/model/user.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String name;
+  final int age;
+
+  User({required this.name, required this.age});
+
+  factory User.fromMap(Map map) {
+    return User(
+      name: map['name'] ?? '',
+      age: map['age'] ?? 0,
+    );
+  }
+
+  factory User.fromFireStore(DocumentSnapshot<Map<String, dynamic>> doc) {
+    Map map = doc.data() ?? {'name': '', 'age': 0};
+
+    return User(
+      name: map['name'],
+      age: map['age'],
+    );
+  }
+}
